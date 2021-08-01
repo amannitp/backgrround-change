812658140d8187ed5bbb50e2ea8c778f2e9878fc
Aman Raj <amannitp272@gmail.com>
2021-08-01 10:55:52 +0530

    first commit

---
+++ background.js
@@ -0,0 +1,16 @@
+let white=document.getElementById('white');
+white.addEventListener('click',function(){
+    document.body.style="background:white;color:black"
+})
+let aqua=document.getElementById('aqua');
+aqua.addEventListener('click',function(){
+    document.body.style="background:aqua;color:black"
+})
+let violet=document.getElementById('violet');
+violet.addEventListener('click',function(){
+    document.body.style="background:violet;color:white"
+})
+let black=document.getElementById('black');
+black.addEventListener('click',function(){
+    document.body.style="background:black;color:white"
+})
